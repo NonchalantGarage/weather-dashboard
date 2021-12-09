b0a2f85a4e0c77eaa5af8ccff8fc3187609dc16a
Curtis <hong.curtis@gmail.com>
2021-12-08 23:20:34 -0800

    adding basing JS

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Study API data - Temp, Wind, Humidity, UV
+
+// setup fetch api data 
+// add event handler for city search 
+
+
+var submitBtnEl = document.querySelector(".btn")
+
+
+// display name function, date and  weather in today's weather container 
+// append most recent search to search history list container 
+
+var citySearchHanlder = function(event){
+    event.preventDefault();
+
+}
+
+
+submitBtnEl.addEventListener("submit", citySearchHandler)
