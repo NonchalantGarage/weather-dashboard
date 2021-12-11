785982f2c2085e02faa15e1c533ec373b08459e1
Curtis <hong.curtis@gmail.com>
2021-12-11 09:48:53 -0800

    append weather attributes for current city

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,10 @@
 
 // pass citysearch to ID, then use One Call API for all the data 
 
+var today = dayjs().format('DD/MM/YYYY');
 var formEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city-search");   
+var currentCityEl = document.querySelector("#current-results");
 
 
 // setup fetch api city data 
@@ -19,12 +21,12 @@ var cityInputEl = document.querySelector("#city-search");
 var getWeatherData = function(city){
 
     // format the api URL
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=8bafed382c7c1d380aab6b5abd6f355a";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=8bafed382c7c1d380aab6b5abd6f355a&units=imperial";
     fetch(apiUrl)
     .then(function(response){
         if (response.ok) {
             response.json().then(function(data){
-                console.log(data);
+                displayCurrentCity(data, city);
             });
         } else {    
             alert("City not found")
@@ -33,6 +35,37 @@ var getWeatherData = function(city){
 
 };
 
+var displayCurrentCity = function(weatherData, citySearch){
+    currentCityEl.textContent = "";
+
+
+    var cityEl = document.createElement("h4");
+    cityEl.textContent = citySearch + " "+ today + " ";
+
+    currentCityEl.appendChild(cityEl);
+
+    // create UL to append li for each weather attribute 
+
+    var listAttributeEl = document.createElement("ul")
+    currentCityEl.appendChild(listAttributeEl)
+
+    var tempEl = document.createElement("li");
+    tempEl.textContent = "Temp: " + weatherData.main.temp + " F";
+    listAttributeEl.appendChild(tempEl)
+
+    
+    var windEl = document.createElement("li");
+    windEl.textContent = "Wind: " + weatherData.wind.speed + " MPH";
+    listAttributeEl.appendChild(windEl);
+    
+    var humidityEl = document.createElement("li")
+    humidityEl.textContent = "Humidity: " + weatherData.main.humidity + " %";
+    listAttributeEl.appendChild(humidityEl)
+
+    var uvEl = document.createElement("li")
+
+    
+}
 
 
 // display name function, date and  weather in today's weather container 
